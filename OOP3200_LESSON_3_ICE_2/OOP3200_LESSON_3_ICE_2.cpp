7c07658f59e58098c0ba8b754768c499c9520822
Taylor Bazouzi <taylor.bazouzi@dcmail.ca>
2021-09-24 22:21:16 -0400

    Add project files.

---
+++ OOP3200_LESSON_3_ICE_2/OOP3200_LESSON_3_ICE_2.cpp
@@ -0,0 +1,14 @@
+//
+// Author: Taylor Bazouzi
+// Student ID: 100579090
+// Date: September 24, 2021
+//
+// File Name: OOP3200_LESSON_3_ICE_2.cpp
+// Description: OOP3200 ICE 3 - Week 3 - Demo
+
+#include <iostream>
+
+int main()
+{
+    
+}
---
+++ OOP3200_LESSON_3_ICE_2/Vector2D.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef __VECTOR_2D__
+#define __VECTOR_2D__
+class Vector2D
+{
+public:
+	// CONSTRUCTOR(S)
+
+	// DESTRUCTOR
+
+	// GETTERS (accessors)
+
+
+	// SETTERS (mutators)
+
+	//  PUBLIC METHODS (public class function)
+
+private:
+	// INSTANCE VARIABLES (class master variables)
+
+	// PRIVATE METHODS (private class functions)
+};
+
+#endif /*defined (__VECTOR_2D__) */
+
