9bd1b62eabb05ae37abd39203ab0ea95de8b3e75
Taylor Bazouzi <taylor.bazouzi@dcmail.ca>
2021-09-24 22:58:02 -0400

    First draft of vector 2D class

--- OOP3200_LESSON_3_ICE_2/Vector2D.cpp
+++ OOP3200_LESSON_3_ICE_2/Vector2D.cpp
@@ -1,1 +1,48 @@
 #include "Vector2D.h"
+
+#include <sstream>
+#include <cmath>
+#include <iostream>
+
+Vector2D::Vector2D()
+{
+	Set(0.0f, 0.0f);
+}
+Vector2D::~Vector2D()
+{
+	
+}
+
+float Vector2D::GetX() const
+{
+	return m_x;
+}
+
+float Vector2D::GetY() const
+{
+	return m_y;
+}
+
+void Vector2D::SetX(const float x = 0.0f)
+{
+	m_x = x;
+}
+
+void Vector2D::SetY(const float y= 0.0f)
+{
+	m_y = y;
+}
+
+void Vector2D::Set(const float x = 0.0f, const float y = 0.0f)
+{
+	SetX(x);
+	SetY(y);
+}
+
+
+std::string Vector2D::ToString() const
+{
+	std::string output_string;
+	output_string += "(" + std::to_string(GetX()) + "," + std::to_string(GetY()) + ")\n";
+	return output_string;
+}
--- OOP3200_LESSON_3_ICE_2/Vector2D.h
+++ OOP3200_LESSON_3_ICE_2/Vector2D.h
@@ -1,22 +1,40 @@
 #pragma once
 #ifndef __VECTOR_2D__
 #define __VECTOR_2D__
+
+#include <string>
+
 class Vector2D
 {
 public:
 	// CONSTRUCTOR(S)
+	Vector2D();
+
+	// COPY CONSTRUCTOR
+	Vector2D(const Vector2D& vector_2d);
+
 
 	// DESTRUCTOR
+	~Vector2D();
 
 	// GETTERS (accessors)
-
+	float GetX() const;
+	float GetY() const;
 
 	// SETTERS (mutators)
+	void SetX(float x);
+	void SetY(float y);
+	void Set(float x, float y);
 
 	//  PUBLIC METHODS (public class function)
+	std::string ToString() const;
 
+	// PUBLIC STATIC METHODS (public class functions that are Static)
+	
 private:
 	// INSTANCE VARIABLES (class master variables)
+	float m_x;
+	float m_y;
 
 	// PRIVATE METHODS (private class functions)
 };
